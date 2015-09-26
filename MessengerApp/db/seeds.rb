22cce04027cea322b1708d961da356bab526c419
prasanthlouis <prasanthlouis21@gmail.com>
2015-09-26 11:04:35 -0400

    Commit

---
+++ MessengerApp/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+m1 = Message.create(content: "Wanna go to the movies?")
+
+m2 = Message.create(content: "Let's meet there!")
